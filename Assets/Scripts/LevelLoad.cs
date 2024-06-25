df1e84f7dd8882362b951f8ab1d8288f4ce94d0a
ManMayBattleCRY <manmaybattlecry@gmail.com>
2024-06-25 16:06:02 +0300

    HideOut

---
+++ Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class LevelLoad : MonoBehaviour
+{
+    public GameObject LoadingScreen;
+    bool isloading = false;
+    public byte SceneID = 1;
+    AsyncOperation Sceneloading;
+    public Image LoadBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+          if (Input.GetKeyDown(KeyCode.F) && !isloading)StartCoroutine(LoadScene()) ;
+    }
+
+
+    IEnumerator LoadScene()
+    {
+        isloading = true;
+        LoadingScreen.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        Sceneloading = SceneManager.LoadSceneAsync(SceneID);
+        while (!Sceneloading.isDone)
+        {
+            LoadBar.fillAmount = Sceneloading.progress;
+            yield return null;
+        }
+        
+    }
+}
